c02ba0f5ec4900e7ae284b14c99eb4b203760cdb
*RLK2lVBOSa <mashimo@monster-dive.com>
2020-09-08 14:27:08 +0900

    [WIP] 編集中

--- opt/got3.py
+++ opt/got3.py
@@ -10,10 +10,17 @@ tweetCriteria = got.manager.TweetCriteria().setQuerySearch(os.environ["TAG"])\
 tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 tweet_lst = []
 users = []
+
+def utc_to_jst(timestamp_utc):
+    datetime_utc = datetime.datetime.strptime(timestamp_utc + "+0000", "%Y-%m-%d %H:%M:%S.%f%z")
+    datetime_jst = datetime_utc.astimezone(datetime.timezone(datetime.timedelta(hours=+9)))
+    timestamp_jst = datetime.datetime.strftime(datetime_jst, '%Y-%m-%d %H:%M:%S.%f')
+    return timestamp_jst
+
 for tweet in tweets:
   tweet_params = []
-  # tweet.date = tweet.date.strftime('%Y年%m月%d日')
-  tweet_params.extend([tweet.date, tweet.username, tweet.text])
+  #　調整中
+  tweet_params.extend([utc_to_jst(tweet.date), tweet.username, tweet.text])
   tweet_lst.append(tweet_params)
   users.append(tweet.username)
 
@@ -23,8 +30,12 @@ users = list(set(users))
 Column =['日付', 'アカウント名', 'ツイート']
 df = pd.DataFrame(tweet_lst, columns=Column)
 
+
+# 調整中
+# filename = "/root/opt/" + {os.environ["TAG"]} + "from" + {(os.environ["FROM"],)} + "to"+ {os.environ["UNTIL"]} + {int(os.environ["NUM"]} + "_" + ".csv"
+
 # # # # # csvの出力
-df.to_csv('/root/opt/tweet_test.csv',encoding='utf_8_sig') # CSV形式で保存
+df.to_csv(filename, encoding='utf_8_sig') # CSV形式で保存
 
 df_users = pd.DataFrame(users)
 
